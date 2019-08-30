72a751ae88cd5ef9faf672371bdc2e5c37c9024e
jihyeseo1120 <mansesjh22@gmail.com>
2019-08-30 16:33:29 +0900

    UI working

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  StatusBar, 
+  TextInput,
+  Dimensions,
+  Platform
+} from 'react-native';
+
+const {height, width} = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content"/>
+      <Text style={styles.title}>My ToDo List</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To Do"}/>
+      </View>
     </View>
   );
 }
@@ -12,8 +26,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#DCE35B',
+    alignItems: 'center'
   },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    marginTop: 50,
+    marginBottom: 30,
+    fontWeight: "200"
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width-25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity : 0.2,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width:0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
+
 });
